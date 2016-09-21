fc9c5d1aaf8a95184811c77362120ce88c20a26c
mistory <598169279@qq.com>
2016-09-21 17:23:23 +0800

    新增loading

--- toast.js
+++ toast.js
@@ -8,6 +8,19 @@
     }
 })(typeof window !== 'undefined' ? window : this, function() {
     'use strict';
+    // 包装一个新的dom，拥有close方法
+    var Node = function(element){
+        this.element = element;
+    }
+    Node.prototype = {
+        constructor:Node,
+        close:function(){
+            alert(1)
+        },
+        clear:function(){
+            
+        }
+    }
 
     var Toast = function(){
         this.handleClick = function(){},
@@ -19,15 +32,13 @@
         version : '0.1.0',
         constructor: Toast,
         alert:function(message){
-            this._render({
+            return this._render({
                 body:message,
-                type:'alert',
-                sure:function(){},
-                cancel:function(){}
+                type:'alert'
             });
         },
         confirm:function(message,sureCallBack,cancelCallBack){
-            this._render({
+            return this._render({
                 body:message,
                 type:'confirm',
                 sure:sureCallBack || function(){},
@@ -35,7 +46,7 @@
             });      
         },
         prompt:function(message,sureCallBack,cancelCallBack){
-            this._render({
+            return this._render({
                 body:'',
                 type:'prompt',
                 sure:sureCallBack || function(){},
@@ -45,17 +56,30 @@
                 }.bind(this)
             });
         },
+        loading:function(message){
+            var node = this._render({
+                body:message || '加载中',
+                type:'loading',
+                sure: function(){},
+                cancel: function(){}
+            });
+            return new Node(node)
+        },
+        clear:function(element){
+            // clear方法清除el
+            this._hide(element)
+        },
         makeText:function(message){
             var toast = this._renderToast(message)
 
             setTimeout(function(){
-                this._hide(toast,function(){});
+                this._hide(toast);
             }.bind(this), 2000)
 
-            return this;
         },
-        _hide:function(element,callback){
+        _hide:function(element){
             this._removeEvent(element);
+
             this._animateEnd(element);
 
             setTimeout(function(){
@@ -71,19 +95,21 @@
             element.classList.remove('in');
         },
         _render:function(options){
+            // dialog
             var div = document.createElement('div');
             div.className = 'toast';
-            div.innerHTML = '<div class="toast-mask"></div><div class="toast-message toast-message-'+options.type+'"><h2>提示</h2><div class="toast-content">'+options.body+'<input type="text"></div><div class="toast-foot"><button data-btn="cancel">取消</button><button data-btn="sure">确定</button></div></div>';
+            div.innerHTML = '<div class="toast-message toast-message-'+options.type+'"><h2>提示</h2><div class="toast-content">'+options.body+'<input type="text"></div><div class="toast-foot"><button data-btn="cancel">取消</button><button data-btn="sure">确定</button></div></div><div class="toast-mask"></div>';
             
             this.handleClick = function(event){
                 var target = event.target;
                 if(target.tagName.toLowerCase()==='button'){
                     if(target.dataset.btn==='cancel'){
-                        this._hide(div,options.cancel());
+                        options.cancel&&options.cancel()
                     }else {
                         this.value = div.getElementsByTagName('input')[0].value;
-                        this._hide(div,options.sure());
+                        options.sure&&options.sure()
                     }
+                    this._hide(div);
                 }
             }.bind(this);
 
@@ -92,8 +118,14 @@
 
             this._animateStart(div);
 
+            // div.__proto__.close = function(){
+            //     alert(1)
+            // }
+            
+            return div;
         },
         _renderToast:function(message){
+            // toast
             var div = document.createElement('div');
             div.className = 'toast-item';
             div.innerHTML = '<div><h2>'+message+'</h2></div>';
@@ -109,6 +141,7 @@
                 var div = document.createElement('div');
                 div.id = 'toast';
                 document.body.appendChild(div);
+                return div;
             }
             return document.getElementById('toast');
         },
@@ -118,15 +151,27 @@
         },
         _removeDom:function(element){
             var parent = element.parentNode;
-            parent.removeChild(element)
-            if(parent.id === 'toast'){
-                remove(this.toasts,element);
-                if(this.toasts.length===0){
-                    document.body.removeChild(document.getElementById('toast'))
+            if(parent!==null){
+                parent.removeChild(element)
+                if(parent.id === 'toast'){
+                    remove(this.toasts,element);
+                    if(this.toasts.length===0){
+                        document.body.removeChild(document.getElementById('toast'))
+                    }
                 }
             }
         }
     }
+
+    var Node = function(element){
+        this.element = element;
+    }
+    Node.prototype = {
+        constructor:Node,
+        close:function(){
+            alert(1)
+        }
+    } 
 
     function remove(array,item){
         var len = array.length;
@@ -137,11 +182,5 @@
         }
     }
 
-    function sleep(time){
-        var start =  new Date();
-        while (new Date()-start<time) {
-            //
-        }
-    }
     return new Toast();
 });
