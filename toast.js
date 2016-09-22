f01cf8083ed07015bcc51f2b531d7ca6a9fa58d8
mistory <598169279@qq.com>
2016-09-22 15:54:33 +0800

    edit

--- toast.js
+++ toast.js
@@ -8,140 +8,145 @@
     }
 })(typeof window !== 'undefined' ? window : this, function() {
     'use strict';
-
-    var Toast = function(){
-        this.handleClick = function(){},
-        this.value = '';
-        this.toasts = [];
-    };
-
-    Toast.prototype = {
-        version : '0.1.0',
-        constructor: Toast,
-        alert:function(message){
-            this._render({
-                body:message,
-                type:'alert',
-                sure:function(){},
-                cancel:function(){}
-            });
+    var toasts = [];
+    var Node = function(element){
+        this.element = element;
+        this.handleClick = function(){};
+        this.value = ''
+    }
+    Node.prototype = {
+        constructor:Node,
+        show:function(){
+            window.getComputedStyle(this.element).top;
+            this.element.classList.add('in');
         },
-        confirm:function(message,sureCallBack,cancelCallBack){
-            this._render({
-                body:message,
-                type:'confirm',
-                sure:sureCallBack || function(){},
-                cancel:cancelCallBack || function(){}
-            });      
-        },
-        prompt:function(message,sureCallBack,cancelCallBack){
-            this._render({
-                body:'',
-                type:'prompt',
-                sure:sureCallBack || function(){},
-                cancel:cancelCallBack || function(){},
-                val:function(){
-                    return this.value;
-                }.bind(this)
-            });
-        },
-        makeText:function(message){
-            var toast = this._renderToast(message)
+        clear:function(){
+            window.getComputedStyle(this.element).top;
+            this.element.classList.remove('in');
 
             setTimeout(function(){
-                this._hide(toast,function(){});
-            }.bind(this), 2000)
-
-            return this;
-        },
-        _hide:function(element,callback){
-            this._removeEvent(element);
-            this._animateEnd(element);
-
-            setTimeout(function(){
-                this._removeDom(element)
+                this._remove()
             }.bind(this), 250)
         },
-        _animateStart:function(element){
-            window.getComputedStyle(element).top;
-            element.classList.add('in');
-        },
-        _animateEnd:function(element){
-            window.getComputedStyle(element).top;
-            element.classList.remove('in');
-        },
-        _render:function(options){
-            var div = document.createElement('div');
-            div.className = 'toast';
-            div.innerHTML = '<div class="toast-mask"></div><div class="toast-message toast-message-'+options.type+'"><h2>提示</h2><div class="toast-content">'+options.body+'<input type="text"></div><div class="toast-foot"><button data-btn="cancel">取消</button><button data-btn="sure">确定</button></div></div>';
-            
+        bind:function(sure,cancel){
+            var self = this;
             this.handleClick = function(event){
                 var target = event.target;
                 if(target.tagName.toLowerCase()==='button'){
                     if(target.dataset.btn==='cancel'){
-                        this._hide(div,options.cancel());
+                        cancel&&cancel()
                     }else {
-                        this.value = div.getElementsByTagName('input')[0].value;
-                        this._hide(div,options.sure());
+                        self.value = self.element.getElementsByTagName('input')[0].value;
+                        sure&&sure()
+                    }
+                    self.clear();
+                }
+            };
+            this.element.addEventListener('click', this.handleClick,false)
+            return this;
+        },
+        val:function(){
+            return this.value;
+        },
+        _remove:function(){
+            this.element.removeEventListener('click',this.handleClick)
+
+            var parent = this.element.parentNode;
+            if(parent!==null){
+                parent.removeChild(this.element)
+                if(parent.id === 'toast'){
+                    remove(toasts,element);
+                    if(toasts.length===0){
+                       document.body.removeChild(document.getElementById('toast'))
                     }
                 }
-            }.bind(this);
-
-            div.addEventListener('click', this.handleClick,false)
+            }
+        }
+    }
+    
+    var toast = function() {
+        function render(options){
+            // dialog
+            var div = document.createElement('div');
+            div.className = 'toast';
+            div.innerHTML = '<div class="toast-message toast-message-'+options.type+'"><h2>提示</h2><div class="toast-content">'+options.body+'<input type="text"></div><div class="toast-foot"><button data-btn="cancel">取消</button><button data-btn="sure">确定</button></div></div><div class="toast-mask"></div>';
             document.body.appendChild(div);
+            return div;
+        }
 
-            this._animateStart(div);
-
-        },
-        _renderToast:function(message){
+        function renderToast(message){
+            // toast
             var div = document.createElement('div');
             div.className = 'toast-item';
             div.innerHTML = '<div><h2>'+message+'</h2></div>';
 
-            this.toasts.push(div);
-            this._parent().appendChild(div);
-            this._animateStart(div);
-
+            toasts.push(div);
+            parent().appendChild(div);
             return div;
-        },
-        _parent:function(){
+        }
+        function parent(){
             if(!document.getElementById('toast')){
                 var div = document.createElement('div');
                 div.id = 'toast';
                 document.body.appendChild(div);
+                return div;
             }
             return document.getElementById('toast');
-        },
-        _removeEvent:function(element){
-            this.value = '';
-            element.removeEventListener('click',this.handleClick)
-        },
-        _removeDom:function(element){
-            var parent = element.parentNode;
-            parent.removeChild(element)
-            if(parent.id === 'toast'){
-                remove(this.toasts,element);
-                if(this.toasts.length===0){
-                    document.body.removeChild(document.getElementById('toast'))
+        }
+
+        function remove(array,item){
+            var len = array.length;
+            while (len--) {
+                if (array[len] === item) {
+                    array.splice(len, 1);
                 }
             }
         }
-    }
 
-    function remove(array,item){
-        var len = array.length;
-        while (len--) {
-            if (array[len] === item) {
-                array.splice(len, 1);
+        return {
+            alert:function(message,sure,cancel){
+                var node = new Node(render({
+                    body:message,
+                    type:'alert'
+                }))
+                node.bind(sure,cancel).show()
+                return node
+            },
+            confirm:function(message,sure,cancel){
+                var node = new Node(render({
+                    body:message,
+                    type:'confirm'
+                }))
+                node.bind(sure,cancel).show()
+                return node   
+            },
+            prompt:function(message,sure,cancel){
+                var node = new Node(render({
+                    body:'',
+                    type:'prompt'
+                }))
+                node.bind(sure,cancel).show()
+                return node 
+            },
+            loading:function(message){
+                var node = new Node(render({
+                    body:message || '加载中',
+                    type:'loading'
+                }))
+                node.show()
+                return node
+            },
+            makeText:function(message){
+                var node = new Node(renderToast(message))
+                node.show()
+                setTimeout(function(){
+                    node.clear();
+                }, 2000)
+                return node
             }
         }
-    }
 
-    function sleep(time){
-        var start =  new Date();
-        while (new Date()-start<time) {
-            //
-        }
-    }
-    return new Toast();
+    }();
+    
+    return toast;
 });
